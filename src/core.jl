cd9806eae0a9e58500db540147d952f2cb3f82a1
charlyalizadeh <charlypro1502@outlook.fr>
2022-02-19 14:24:53 +0100

    Add function to check the validity of the network

--- src/core.jl
+++ src/core.jl
@@ -1,3 +1,7 @@
 nbus(network::PowerFlowNetwork) = size(network.bus, 1)
 nbranch(network::PowerFlowNetwork) = size(network.branch, 1)
 ngen(network::PowerFlowNetwork) = size(network.gen, 1)
+is_disjoint(network::PowerFlowNetwork) = nbranch(network) < nbus(network) - 1
+has_bus(network::PowerFlowNetwork) = nbus(network) > 0
+has_branch(network::PowerFlowNetwork) = nbranch(network) > 0
+has_gen(network::PowerFlowNetwork) = ngen(network) > 0
