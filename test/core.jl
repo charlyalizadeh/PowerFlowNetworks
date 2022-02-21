2bcf0b410c92f8660fdcfe1fb08b394e9eb02a00
charlyalizadeh <charlypro1502@outlook.fr>
2022-02-21 12:01:35 +0100

    Update tests

--- test/core.jl
+++ test/core.jl
@@ -4,4 +4,12 @@
         @test nbus(network) == 6
         @test nbranch(network) == 11
         @test ngen(network) == 3
+
+        @test has_continuous_index(network) == true
+        network.bus = network.bus[1:end .!= 2, :]
+        @test has_continuous_index(network) == false
+
+        @test is_disjoint(network) == false
+        network.branch = zeros(0, 0)
+        @test is_disjoint(network) == true
 end
