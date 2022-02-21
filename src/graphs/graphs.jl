6641336a591addfa29ec98b6a430384fcdbe0a89
charlyalizadeh <charlypro1502@outlook.fr>
2022-02-21 12:01:19 +0100

    Add normalizing the index of the network before building the graph

--- src/graphs/graphs.jl
+++ src/graphs/graphs.jl
@@ -1,4 +1,8 @@
 function to_simple_graph(network::PowerFlowNetwork)
+    if !has_continuous_index(network)
+        network = deepcopy(network)
+        normalize_index!(network)
+    end
     g = SimpleGraph(nbus(network))
     for (src, dst) in eachrow(network.branch[begin:end, 1:2])
         add_edge!(g, src, dst)
