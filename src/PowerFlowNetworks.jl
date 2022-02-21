ee56d9e3fcf1d702faa04b4bcd2b13508a548fff
charlyalizadeh <charlypro1502@outlook.fr>
2022-02-21 11:58:18 +0100

    Add functions to check the validity of a `PowerFlowNetwork`

--- src/PowerFlowNetworks.jl
+++ src/PowerFlowNetworks.jl
@@ -2,7 +2,7 @@ module PowerFlowNetworks
 
 using Graphs: SimpleGraph, add_edge!
 
-struct PowerFlowNetwork
+mutable struct PowerFlowNetwork
     bus::Matrix{Float64}
     gen::Matrix{Float64}
     branch::Matrix{Float64}
@@ -10,6 +10,7 @@ struct PowerFlowNetwork
     baseMVA::Float64
 end
 
+include("utils.jl")
 include("core.jl")
 include("io/read.jl")
 include("graphs/graphs.jl")
@@ -18,7 +19,8 @@ include("graphs/graphs.jl")
 PowerFlowNetwork(path::AbstractString; format::AbstractString) = read_network(path; format=format)
 SimpleGraph(network::PowerFlowNetwork) = to_simple_graph(network)
 
-export nbus, nbranch, ngen
+export nbus, nbranch, ngen, is_disjoint, has_bus, has_branch, has_gen,
+       has_continuous_index, normalize_index
 export PowerFlowNetwork
 export Graph
 
--- src/core.jl
+++ src/core.jl
@@ -1,7 +1,27 @@
 nbus(network::PowerFlowNetwork) = size(network.bus, 1)
+
 nbranch(network::PowerFlowNetwork) = size(network.branch, 1)
+
 ngen(network::PowerFlowNetwork) = size(network.gen, 1)
+
 is_disjoint(network::PowerFlowNetwork) = nbranch(network) < nbus(network) - 1
+
 has_bus(network::PowerFlowNetwork) = nbus(network) > 0
+
 has_branch(network::PowerFlowNetwork) = nbranch(network) > 0
+
 has_gen(network::PowerFlowNetwork) = ngen(network) > 0
+
+function has_continuous_index(network::PowerFlowNetwork; startone=true)
+    idx = network.bus[:, 1]
+    idx == idx[1]: idx[end] && (idx[1] == 1 || !startone)
+end
+
+
+function normalize_index!(network::PowerFlowNetwork)
+    idx_map = [Pair(i, j) for (i, j) in zip(network.bus[:, 1], 1:nbus(network))]
+    network.bus[:, 1] = replace(network.bus[:, 1], idx_map...)
+    network.gen[:, 1] = replace(network.gen[:, 1], idx_map...)
+    network.branch[:, 1] = replace(network.branch[:, 1], idx_map...)
+    network.branch[:, 2] = replace(network.branch[:, 2], idx_map...)
+end
