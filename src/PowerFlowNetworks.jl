c3a32c1f99411d6db636ca7d90fafa41b9b0724a
charlyalizadeh <charlypro1502@outlook.fr>
2022-03-07 14:20:57 +0100

    Add exporting `serialize_instances!`

--- src/PowerFlowNetworks.jl
+++ src/PowerFlowNetworks.jl
@@ -29,7 +29,7 @@ SimpleGraph(network::PowerFlowNetwork) = to_simple_graph(network)
 export nbus, nbranch, ngen, is_disjoint, has_bus, has_branch, has_gen,
        has_continuous_index, normalize_index
 export nbranch_unique, ngen_unique
-export save_features_instances!, save_basic_features_instances!, save_single_features_instances!
+export save_features_instances!, save_basic_features_instances!, save_single_features_instances!, serialize_instances!
 export PowerFlowNetwork
 export Graph
 export setup_db
