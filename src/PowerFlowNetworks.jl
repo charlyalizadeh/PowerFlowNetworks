7cb0f908a31a665d29602a2114c386df3a17aea5
charlyalizadeh <charlypro1502@outlook.fr>
2022-03-09 13:54:16 +0100

    Update includes and exports

--- src/PowerFlowNetworks.jl
+++ src/PowerFlowNetworks.jl
@@ -5,6 +5,8 @@ using SQLite, DataFrames
 using Dates
 using Statistics: mean
 using Serialization
+using DataStructures
+using Random
 
 mutable struct PowerFlowNetwork
     bus::DataFrame
@@ -15,9 +17,11 @@ end
 
 
 include("core.jl")
+include("utils/graphs/ischordal.jl")
 include("io/read.jl")
 include("read_features.jl")
 include("graphs/graphs.jl")
+include("graphs/operations.jl")
 include("db/setup_db.jl")
 include("db/inserts.jl")
 include("db/operations.jl")
@@ -27,11 +31,13 @@ PowerFlowNetwork(path::AbstractString, format::AbstractString) = read_network(pa
 SimpleGraph(network::PowerFlowNetwork) = to_simple_graph(network)
 
 export nbus, nbranch, ngen, is_disjoint, has_bus, has_branch, has_gen,
-       has_continuous_index, normalize_index
+       has_continuous_index, normalize_index, merge_duplicate_branch!
+export ischordal
 export nbranch_unique, ngen_unique
 export save_features_instances!, save_basic_features_instances!, save_single_features_instances!, serialize_instances!
 export PowerFlowNetwork
 export Graph
+export add_edges_distance!, add_edge_random!
 export setup_db
 export load_instance!
 
