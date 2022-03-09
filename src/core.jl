5e1db7a51fde8fff19b6e4299e47d0000cd3aefa
charlyalizadeh <charlypro1502@outlook.fr>
2022-03-09 13:51:57 +0100

    Add merging duplicate branches

--- src/core.jl
+++ src/core.jl
@@ -8,6 +8,8 @@ function nbranch(network::PowerFlowNetwork; distinct_pair=false)
     end
 end
 
+nbranch_unique(network::PowerFlowNetwork) = nbranch(network; distinct_pair=true)
+
 ngen(network::PowerFlowNetwork) = nrow(network.gen)
 
 is_disjoint(network::PowerFlowNetwork) = nbranch(network) < nbus(network) - 1
@@ -29,4 +31,24 @@ function normalize_index!(network::PowerFlowNetwork)
     network.gen[!, :ID] = replace(network.gen[!, :ID], idx_map...)
     network.branch[!, :SRC] = replace(network.branch[!, :SRC], idx_map...)
     network.branch[!, :DST] = replace(network.branch[!, :DST], idx_map...)
+end
+
+function merge_duplicate_branch!(network::PowerFlowNetwork)
+    gdf = groupby(network.branch, [:SRC, :DST])
+    network.branch = combine(gdf,
+                             :BR_R => mean,
+                             :BR_X => mean,
+                             :BR_B => sum,
+                             :RATE_A => mean,
+                             :RATE_B => mean,
+                             :RATE_C => mean,
+                             :ANGMIN => minimum,
+                             :ANGMAX => maximum,
+                             :PF => sum,
+                             :QT => sum,
+                             :PT => sum,
+                             :MU_SF => mean,
+                             :MU_ST => mean,
+                             :MU_ANGMIN => minimum,
+                             :MU_ANGMAX => maximum)
 end
