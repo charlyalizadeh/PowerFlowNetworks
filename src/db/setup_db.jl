7f3f976c2d44f970ab940714f7401b86adaf9874
charlyalizadeh <charlypro1502@outlook.fr>
2022-03-10 14:07:28 +0100

    Add median and variance to population variable

--- src/db/setup_db.jl
+++ src/db/setup_db.jl
@@ -6,6 +6,7 @@ function create_instances_table(db)
         source_path TEXT NOT NULL,
         source_type TEXT NOT NULL,
         source_pfn TEXT,
+        source_graph TEXT,
         mat_path TEXT,
         ctr_path TEXT,
         date TEXT NOT NULL,
@@ -14,31 +15,31 @@ function create_instances_table(db)
 
         nb_edge INTEGER,
         nb_vertex INTEGER,
-        degree_max INTEGER, degree_min INTEGER, degree_mean REAL,
+        degree_max INTEGER, degree_min INTEGER, degree_mean REAL, degree_median REAL, degree_var REAL,
         global_clustering_coefficient REAL,
         density REAL,
         diameter INTEGER,
         radius INTEGER,
 
-        PD_max REAL, PD_min REAL, PD_mean REAL,
-        QD_max REAL, QD_min REAL, QD_mean REAL,
-        GS_max REAL, GS_min REAL, GS_mean REAL,
-        BS_max REAL, BS_min REAL, BS_mean REAL,
-        VM_max REAL, VM_min REAL, VM_mean REAL,
-        VA_max REAL, VA_min REAL, VA_mean REAL,
-        VMAX_max REAL, VMAX_min REAL, VMAX_mean REAL,
-        VMIN_max REAL, VMIN_min REAL, VMIN_mean REAL,
+        PD_max REAL, PD_min REAL, PD_mean REAL, PD_median REAL, PD_var REAL,
+        QD_max REAL, QD_min REAL, QD_mean REAL, QD_median REAL, QD_var REAL,
+        GS_max REAL, GS_min REAL, GS_mean REAL, GS_median REAL, GS_var REAL,
+        BS_max REAL, BS_min REAL, BS_mean REAL, BS_median REAL, BS_var REAL,
+        VM_max REAL, VM_min REAL, VM_mean REAL, VM_median REAL, VM_var REAL,
+        VA_max REAL, VA_min REAL, VA_mean REAL, VA_median REAL, VA_var REAL,
+        VMAX_max REAL, VMAX_min REAL, VMAX_mean REAL, VMAX_median REAL, VMAX_var REAL,
+        VMIN_max REAL, VMIN_min REAL, VMIN_mean REAL, VMIN_median REAL, VMIN_var REAL,
 
-        PG_max REAL, PG_min REAL, PG_mean REAL,
-        QG_max REAL, QG_min REAL, QG_mean REAL,
-        QMAX_max REAL, QMAX_min REAL, QMAX_mean REAL,
-        QMIN_max REAL, QMIN_min REAL, QMIN_mean REAL,
-        PMAX_max REAL, PMAX_min REAL, PMAX_mean REAL,
-        PMIN_max REAL, PMIN_min REAL, PMIN_mean REAL,
+        PG_max REAL, PG_min REAL, PG_mean REAL, PG_median REAL, PG_var REAL,
+        QG_max REAL, QG_min REAL, QG_mean REAL, QG_median REAL, QG_var REAL,
+        QMAX_max REAL, QMAX_min REAL, QMAX_mean REAL, QMAX_median REAL, QMAX_var REAL,
+        QMIN_max REAL, QMIN_min REAL, QMIN_mean REAL, QMIN_median REAL, QMIN_var REAL,
+        PMAX_max REAL, PMAX_min REAL, PMAX_mean REAL, PMAX_median REAL, PMAX_var REAL,
+        PMIN_max REAL, PMIN_min REAL, PMIN_mean REAL, PMIN_median REAL, PMIN_var REAL,
 
-        BR_R_max REAL, BR_R_min REAL, BR_R_mean REAL,
-        BR_X_max REAL, BR_X_min REAL, BR_X_mean REAL,
-        BR_B_max REAL, BR_B_min REAL, BR_B_mean REAL,
+        BR_R_max REAL, BR_R_min REAL, BR_R_mean REAL, BR_R_median REAL, BR_R_var REAL,
+        BR_X_max REAL, BR_X_min REAL, BR_X_mean REAL, BR_X_median REAL, BR_X_var REAL,
+        BR_B_max REAL, BR_B_min REAL, BR_B_mean REAL, BR_B_median REAL, BR_B_var REAL,
 
         PRIMARY KEY(name, scenario)
     )
@@ -50,10 +51,12 @@ function create_decompositions_table(db)
     createtable_query = """
     CREATE TABLE IF NOT EXISTS decompositions(
         id INTEGER NOT NULL,
+        uuid TEXT NOT NULL,
         origin_name TEXT NOT NULL,
         origin_scenario INTEGER NOT NULL,
 
-        pre_process_path TEXT,
+        extension_alg TEXT,
+        preprocess_path TEXT,
         date TEXT,
 
         clique_path TEXT,
@@ -62,15 +65,15 @@ function create_decompositions_table(db)
         nb_added_edge_dec INTEGER,
         nb_edge INTEGER,
         nb_vertex INTEGER,
-        degree_max INTEGER, degree_min INTEGER, degree_mean REAL,
+        degree_max INTEGER, degree_min INTEGER, degree_mean REAL, degree_median REAL, degree_var REAL,
         global_clustering_coefficient REAL,
         density REAL,
         diameter INTEGER,
         radius INTEGER,
 
         nb_clique INTEGER,
-        clique_size_max INTEGER, clique_size_min INTEGER, clique_size_mean REAL, clique_size_var REAL,
-        nb_lnkctr INTEGER,
+        cliques_size_max INTEGER, cliques_size_min INTEGER, cliques_size_mean REAL, cliques_size_median REAL, cliques_size_var REAL,
+        nb_lc INTEGER,
 
         PRIMARY KEY(id),
         FOREIGN KEY(origin_name, origin_scenario) REFERENCES instances(name, scenario)
