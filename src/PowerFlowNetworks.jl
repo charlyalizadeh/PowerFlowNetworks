9b2946dc624da0bc54fa35cdf05d548fcbdec096
charlyalizadeh <charlypro1502@outlook.fr>
2022-03-14 11:59:40 +0100

    Update the includes and exports

--- src/PowerFlowNetworks.jl
+++ src/PowerFlowNetworks.jl
@@ -26,6 +26,7 @@ include("core.jl")
 include("utils/graphs/lbfs.jl")
 include("utils/graphs/ischordal.jl")
 include("utils/graphs/build_graph.jl")
+include("utils/clique_cliquetree.jl")
 include("io/read.jl")
 include("read_features.jl")
 include("graphs/graphs.jl")
@@ -35,6 +36,7 @@ include("db/infos.jl")
 include("db/inserts.jl")
 include("utils/graphs/operators.jl")
 include("decompositions/chordal_extension/chordal_extension.jl")
+include("decompositions/merge/merge.jl")
 include("db/operations.jl")
 
 
@@ -52,7 +54,9 @@ export setup_db
 export has_opf_tables, state_columns
 export load_instance!
 export chordal_extension
+export merge_dec
 export save_features_instances!, save_basic_features_instances!, save_single_features_instances!,
-       serialize_instances!, generate_decompositions!
+       serialize_instances!, generate_decompositions!, merge_decompositions!, check_chordality_decompositions,
+       check_connectivity_instances, check_selfloops
 
 end
