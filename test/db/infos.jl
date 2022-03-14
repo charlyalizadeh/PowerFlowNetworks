937609705f3815e2ecc00f78677dd744b7356121
charlyalizadeh <charlypro1502@outlook.fr>
2022-03-15 00:21:30 +0100

    Update test

--- test/db/infos.jl
+++ test/db/infos.jl
@@ -2,10 +2,14 @@
     db = SQLite.DB("test/data/TEST_PowerFlowNetworks_SQLite.sqlite")
     SQLite.drop!(db, "instances")
     @test !has_opf_tables(db)
-    db = setup_db("test/data/TEST_PowerFlowNetworks_SQLite.sqlite")
+    db = setup_db("test/data/TEST_PowerFlowNetworks_SQLite.sqlite"; delete_if_exists=true)
+    @test table_count(db, "instances") == 0
+    @test table_count(db, "decompositions") == 0
+    @test table_count(db, "merges") == 0
+    @test table_count(db, "combinations") == 0
     @test has_opf_tables(db)
     DBInterface.execute(db, "DELETE FROM instances")
-    states = state_columns(db, "instances", ["name", "scenario", "nbus", "nbranch", "nbranch_unique", "ngen"])
+    states = count_missing_columns(db, "instances", ["name", "scenario", "nbus", "nbranch", "nbranch_unique", "ngen"])
     @test collect(values(states)) == zeros(Int, length(states))
 
     name = "case6ww"
@@ -14,9 +18,13 @@
     source_type = "MATPOWER-M"
     date = Dates.now()
     load_instance!(db, name, scenario, source_path, source_type, date)
+    @test table_count(db, "instances") == 1
+    @test table_count(db, "decompositions") == 0
+    @test table_count(db, "merges") == 0
+    @test table_count(db, "combinations") == 0
 
 
-    states = state_columns(db, "instances", ["name", "scenario", "nbus", "nbranch", "nbranch_unique", "ngen"])
+    states = count_missing_columns(db, "instances", ["name", "scenario", "nbus", "nbranch", "nbranch_unique", "ngen"])
     @test states == Dict("name" => 0,
                          "scenario" => 0,
                          "nbus" => 1,
@@ -27,10 +35,10 @@
                          "total" => 1)
 
     save_basic_features_instances!(db)
-    states = state_columns(db, "instances", ["name", "scenario", "nbus", "nbranch", "nbranch_unique", "ngen"])
+    states = count_missing_columns(db, "instances", ["name", "scenario", "nbus", "nbranch", "nbranch_unique", "ngen"])
     @test collect(values(states)) == zeros(Int, length(states))
 
-    states = state_columns(db, "instances", ["global_clustering_coefficient"])
+    states = count_missing_columns(db, "instances", ["global_clustering_coefficient"])
     @test states["global_clustering_coefficient"] == 1
     @test states["total"] == 1
 end
--- test/db/operations.jl
+++ test/db/operations.jl
@@ -1,10 +1,22 @@
 @testset "DB operations" begin
-    db = SQLite.DB("test/data/TEST_PowerFlowNetworks_SQLite.sqlite")
+    db = setup_db("test/data/TEST_PowerFlowNetworks_SQLite.sqlite"; delete_if_exists=true)
+    load_instance!(db, "case6ww", 0, "test/data/case6ww.m", "MATPOWER-M", Dates.now())
+    load_instance!(db, "C2FEN02312", 3, "test/data/C2FEN02312_3.raw", "RAWGO", Dates.now())
+    load_instance!(db, "C2S6N02045", 1, "test/data/C2S6N02045_1.raw", "RAWGO", Dates.now())
+    @test table_count(db, "instances") == 3
+
     save_basic_features_instances!(db)
     save_features_instances!(db)
     serialize_instances!(db, "test/data/networks_serialized", "test/data/graphs")
     generate_decompositions!(db, "test/data/cliques", "test/data/cliquetrees", "test/data/graphs",
-                             "cholesky", "test/data/configs/preprocess_default.json")
+                             "cholesky", "test/data/configs/preprocess_default.json";
+                             subset=[("C2FEN02312", 3), ("case6ww", 0)])
+    @test table_count(db, "decompositions") == 2
     generate_decompositions!(db, "test/data/cliques", "test/data/cliquetrees", "test/data/graphs",
-                             "minimum_degree", "test/data/configs/preprocess_add_random_50.json")
+                             "minimum_degree", "test/data/configs/preprocess_add_random_50.json";
+                             subset=[("C2S6N02045", 1)])
+    @test table_count(db, "decompositions") == 3
+    merge_decompositions!(db, ["molzahn"], [0], "cliques_nv_up", Dict("treshold_percent" => 0.5); subset=[1, 2])
+    @test table_count(db, "decompositions") == 5
+    @test table_count(db, "merges") == 2
 end
