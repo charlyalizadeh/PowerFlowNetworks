554a1f44091f35eedbce33db029d8a3acca0919c
charlyalizadeh <charlypro1502@outlook.fr>
2022-03-14 12:00:58 +0100

    Remove serializing options

--- scripts/read_features.jl
+++ scripts/read_features.jl
@@ -10,13 +10,6 @@ function parse_commandline()
         "--dbpath", "-p"
             help = "Path of the sqlite database file."
             default = "data/PowerFlowNetworks.sqlite"
-        "--serialize"
-            help = "Wether to serialize or not."
-            default = false
-            action = :store_true
-        "--serialize_path"
-            help = "Where to store the serialize networks."
-            default = "data/networks_serialize/"
         "--min_nv"
             help = "Minimum number of vertices a network had to have to be processed."
             arg_type = Int
@@ -37,7 +30,6 @@ function main()
     parsed_args = parse_commandline()
     db = SQLite.DB(parsed_args["dbpath"])
     save_features_instances!(db;
-                             serialize_network=parsed_args["serialize"], serialize_path=parsed_args["serialize_path"],
                              min_nv=parsed_args["min_nv"], max_nv=parsed_args["max_nv"],
                              recompute=parsed_args["recompute"])
 end
