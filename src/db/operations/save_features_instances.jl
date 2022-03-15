6420b7db3441045e1f69aed41b106c2f5505ee6b
charlyalizadeh <charlypro1502@outlook.fr>
2022-03-15 16:46:26 +0100

    Add filter the instance(s) to process

--- src/db/operations/save_features_instances.jl
+++ src/db/operations/save_features_instances.jl
@@ -22,10 +22,14 @@ function save_features_instance_dfrow!(db::SQLite.DB, row)
     save_features_instance!(db, row[:name], row[:scenario], row[:source_type], row[:source_path])
 end
 
-function save_features_instances!(db::SQLite.DB; min_nv=typemin(Int), max_nv=typemax(Int), recompute=false)
+function save_features_instances!(db::SQLite.DB; min_nv=typemin(Int), max_nv=typemax(Int), recompute=false, subset=nothing)
     query = "SELECT name, scenario, source_type, source_path FROM instances WHERE nbus >= $min_nv AND nbus <= $max_nv"
     if !recompute
         query *= " AND nb_edge IS NULL"
+    end
+    if !isnothing(subset)
+        subset = ["('$(s[1])', $(s[2]))" for s in subset]
+        query *= " AND (name, scenario) IN ($(join(subset, ',')))"
     end
     results = DBInterface.execute(db, query) |> DataFrame
     @info "Saving instance features config: min_nv=$min_nv, max_nv=$max_nv, recompute=$recompute"
