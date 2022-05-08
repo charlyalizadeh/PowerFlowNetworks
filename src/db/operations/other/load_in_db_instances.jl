10602c557218098af5461792aa058ea44b7e12e2
charlyalizadeh <charlypro1502@outlook.fr>
2022-05-08 22:44:36 +0200

    Reorganize the `operations` folder

---
+++ src/db/operations/other/load_in_db_instances.jl
@@ -0,0 +1,73 @@
+function load_rawgo_instance!(db::SQLite.DB, path::AbstractString)
+    date = Dates.now()
+    name = splitdir(path)[end]
+    scenarios = readdir(path)
+    @info "Loading $(name)"
+    for scenario in scenarios
+        !isdir(joinpath(path, scenario)) && continue
+        scenario_nb = parse(Int, scenario[10:end])
+        source_path = abspath(joinpath(path, scenario))
+        @info "  $(scenario_nb)"
+        try
+            load_instance_in_db!(db, name, scenario_nb, source_path, "RAWGO", date)
+        catch e
+            if isa(e, SQLiteException) && e.msg == "UNIQUE constraint failed: instances.name, instances.scenario"
+                @warn "$(name) scenario $(scenario_nb) already in the database. Loading ignored."
+            else
+                rethrow()
+            end
+        end
+    end
+end
+
+function load_rawgo_instances!(db::SQLite.DB, dirs)
+    for dir in dirs
+        for instance_dir in readdir(dir; join=true)
+            !isdir(instance_dir) && continue
+            load_rawgo_instance!(db, instance_dir)
+        end
+    end
+end
+
+function load_matpower_mat_instance!(db::SQLite.DB, path::AbstractString)
+    date = Dates.now()
+    name = splitext(basename(path))[end - 1]
+    scenario_nb = 0
+    source_path = abspath(path)
+    source_type = "MATPOWERM"
+    @info "Loading $(name)"
+    try
+        load_instance_in_db!(db, name, scenario_nb, source_path, source_type, date)
+    catch e
+        if isa(e, SQLiteException) && e.msg == "UNIQUE constraint failed: instances.name, instances.scenario"
+            @warn "$(name) scenario $(scenario_nb) already in the database. Loading ignored."
+        else
+            rethrow()
+        end
+    end
+end
+
+function load_matpower_mat_instances!(db::SQLite.DB, dirs)
+    for dir in dirs
+        files = readdir(dir; join=true)
+        for f in files
+            !startswith(basename(f), "case") && continue
+            load_matpower_mat_instance!(db, f)
+        end
+    end
+end
+
+function clean_dirs(dirs; verbose=true)
+    cleaned_dirs = [d for d in dirs if isdir(d)]
+    if length(cleaned_dirs) < length(dirs)
+        @warn "$(setdiff(dirs, cleaned_dirs)) are not valid directory/ies (skipped)."
+    end
+    return cleaned_dirs
+end
+
+function load_in_db_instances!(db::SQLite.DB, indirs_rawgo, indirs_matpowerm)
+    indirs_rawgo = clean_dirs(indirs_rawgo)
+    indirs_matpowerm = clean_dirs(indirs_matpowerm)
+    load_matpower_mat_instances!(db, indirs_matpowerm)
+    load_rawgo_instances!(db, indirs_rawgo)
+end
---
+++ src/db/operations/solve/solve_decompositions.jl
@@ -0,0 +1,41 @@
+function solve_decomposition!(db::SQLite.DB, id, origin_name, origin_scenario, clique_path, cliquetree_path, mat_path, ctr_path)
+    println("Solving ($origin_name, $origin_scenario, $id)")
+    cliques = read_clique(clique_path)
+    cliquetree = read_cliquetree(cliquetree_path)
+    instance_name = "$(origin_name)_$(origin_scenario)"
+    solving_time, nb_iter, solve_log_path, objective, m, nb_lc = solve_sdp(instance_name, cliques, cliquetree, ctr_path, mat_path)
+    query = """
+    UPDATE decompositions SET solving_time=$solving_time, nb_iter=$nb_iter, solve_log_path='$solve_log_path', objective=$objective, m=$m, nb_lc=$nb_lc
+    WHERE id = $id
+    """
+    execute_query(db, query)
+end
+
+
+function solve_decomposition_dfrow!(db::SQLite.DB, id, origin_id, origin_name, origin_scenario, clique_path, cliquetree_path, mat_paths, ctr_paths)
+    if !haskey(mat_paths, origin_id) || !haskey(ctr_paths, origin_id) || ismissing(ctr_paths[origin_id]) || ismissing(mat_paths[origin_id])
+        println("No matctr files registered in the database for ($origin_name, $origin_scenario).")
+    else
+        solve_decomposition!(db, id, origin_name, origin_scenario, clique_path, cliquetree_path, mat_paths[origin_id], ctr_paths[origin_id])
+    end
+end
+
+
+
+function solve_decompositions!(db::SQLite.DB; subset=nothing)
+    println("Solving decompositions")
+    println("subset\n$subset\nend subset")
+    query = "SELECT id, origin_id, origin_name, origin_scenario, clique_path, cliquetree_path FROM decompositions"
+    if !isnothing(subset)
+        query *= " WHERE id IN ($(join(subset, ',')))"
+    end
+    results = DBInterface.execute(db, query) |> DataFrame
+    # Retrieving mat and ctr path
+    query = "SELECT id, mat_path, ctr_path FROM instances"
+    matctr_paths = DBInterface.execute(db, query) |> DataFrame
+    mat_paths = Dict(Pair.(matctr_paths[!, :id], matctr_paths[!, :mat_path]))
+    ctr_paths = Dict(Pair.(matctr_paths[!, :id], matctr_paths[!, :ctr_path]))
+    solve_decomposition_func!(row) = solve_decomposition_dfrow!(db, row[:id], row[:origin_id], row[:origin_name], row[:origin_scenario],
+                                                                row[:clique_path], row[:cliquetree_path], mat_paths, ctr_paths)
+    solve_decomposition_func!.(eachrow(results[!, [:id, :origin_id, :origin_name, :origin_scenario, :clique_path, :cliquetree_path]]))
+end
