458be9f56f8ae7c2ff89cebdf82e2531b0758a57
charlyalizadeh <charlypro1502@outlook.fr>
2022-05-13 12:10:42 +0200

    Add function to convert piecewise linear costs to polynomial costs

--- src/core.jl
+++ src/core.jl
@@ -25,6 +25,22 @@ function has_continuous_index(network::PowerFlowNetwork; startone=true)
     idx == idx[1]: idx[end] && (idx[1] == 1 || !startone)
 end
 
+function get_cost_type(network::PowerFlowNetwork)
+    if isempty(network.gencost)
+        return "empty"
+    end
+    cost_type = network.gencost[!, :MODEL]
+    if all(cost_type .== 1)
+        return "piecewise linear"
+    elseif all(cost_type .== 2)
+        return "polynomial"
+    elseif all(cost_type .== 1 .|| a.== 2)
+        error("Mixed cost types for $(network.name)")
+    else
+        error("Cost types of $(network.name) contains values not equal to 1 or 2.")
+    end
+end
+
 function normalize_index!(network::PowerFlowNetwork)
     idx_map = [Pair(i, j) for (i, j) in zip(network.bus[!, :ID], 1:nbus(network))]
     network.bus[!, :ID] = replace(network.bus[!, :ID], idx_map...)
@@ -55,7 +71,45 @@ end
 
 function set_index_gencost!(network::PowerFlowNetwork)
     if size(network.gencost, 1) != size(network.gen, 1)
-        error("The network $(network.name) doesn't have the same dimension for `gencost` and `gen`. Cannot set the index for `gencost`.")
+        @warn "The network $(network.name) doesn't have the same dimension for `gencost` and `gen`. Cannot set the index for `gencost`."
+    else
+        insertcols!(network.gencost, 1, :ID => network.gen[!, :ID])
     end
-    network.gencost[!, :ID] = network.gen[!, :ID]
+end
+
+function _convert_gencost_to_polynomial_dfrow(row, order=2)
+    ncost = row[4]
+    coefficients = row[5: 4 + 2 * ncost]
+    points = [[p, c] for (p, c) in zip(coefficients[1:2:end], coefficients[2:2:end])]
+    sort!(points, by=x->x[2])
+    power = [p[1] for p in points]
+    costs = [p[2] for p in points]
+    for i in length(power):-1:2
+        if power[i] <= power[i - 1]
+            power[i - 1] = power[i] - 1
+        end
+    end
+    polynome = fit(power, costs, order)
+    new_coeffs = reverse(coeffs(polynome))
+    if new_coeffs == [0]
+        new_coeffs = zeros(Float64, order + 1)
+    end
+    return vcat([2], row[2], row[3], [order + 1], new_coeffs)
+end
+
+function convert_gencost_to_polynomial!(network::PowerFlowNetwork, order=2)
+    convert_function(row) = _convert_gencost_to_polynomial_dfrow(row, order)
+    new_rows = convert_function.(eachrow(network.gencost))
+    new_rows = reduce(hcat, new_rows)'
+    network.gencost = DataFrame(new_rows, vcat(["MODEL", "STARTUP", "SHUTDOWN", "NCOST"], ["COST$i" for i in 1:order+1]))
+    network.gencost[!, :MODEL] = floor.(Int, network.gencost[!, :MODEL])
+    network.gencost[!, :NCOST] = floor.(Int, network.gencost[!, :NCOST])
+end
+
+function convert_gencost!(network::PowerFlowNetwork, to)
+    if to == "polynomial"
+        convert_gencost_to_polynomial!(network)
+    elseif to == "piecewise linear"
+        error("Not implemented")
+    end
 end
