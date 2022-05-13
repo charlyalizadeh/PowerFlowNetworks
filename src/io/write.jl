909aa78728288c5f21655455effc16cfee7dedf0
charlyalizadeh <charlypro1502@outlook.fr>
2022-05-13 12:15:30 +0200

    Use custom function instead of `CSV.write` when writing a network to a `.m` file

--- src/io/write.jl
+++ src/io/write.jl
@@ -2,6 +2,24 @@ function format_col(col, val)
     return col == 1 ? "\t$val" : val
 end
 
+
+function _get_number_rep(n)
+    if isinteger(n) 
+        @sprintf "%.0f" n 
+    else
+        @sprintf "%.10f" n
+    end
+end
+
+function _df2matpowermcsv_dfrow(row)
+    row = [_get_number_rep(v) for v in row]
+    return "\t" * join(row, "\t") * ";\n"
+end
+
+function _df2matpowermcsv(df)
+    df_str = join(_df2matpowermcsv_dfrow.(eachrow(df))) 
+end
+
 function write_pfn(io::IO, network::PowerFlowNetwork)
     text = """function mpc = $(network.name)\n
 %% MATPOWER Case Format : Version 2
@@ -14,36 +32,24 @@ mpc.baseMVA = $(network.baseMVA);
 %% bus data
 %	bus_i	type	Pd	Qd	Gs	Bs	area	Vm	Va	baseKV	zone	Vmax	Vmin
 mpc.bus = [
-\t"""
-    write(io, text)
-    CSV.write(io, network.bus; delim='\t', append=true, newline=";\n\t")
-    text = """];
+$(_df2matpowermcsv(network.bus))];
 
 %% generator data
 %	bus	Pg	Qg	Qmax	Qmin	Vg	mBase	status	Pmax	Pmin	Pc1	Pc2	Qc1min	Qc1max	Qc2min	Qc2max	ramp_agc	ramp_10	ramp_30	ramp_q	apf
 mpc.gen = [
-\t"""
-    write(io, text)
-    CSV.write(io, network.gen; delim='\t', append=true, newline=";\n\t")
-    text = """];
+$(_df2matpowermcsv(network.gen))];
 
 %% branch data
 %	fbus	tbus	r	x	b	rateA	rateB	rateC	ratio	angle	status	angmin	angmax
 mpc.branch = [
-\t"""
-    write(io, text)
-    CSV.write(io, network.branch; delim='\t', append=true, newline=";\n\t")
-    text = """];
+$(_df2matpowermcsv(network.branch))];
 
 %%-----  OPF Data  -----%%
 %% generator cost data
 %	1	startup	shutdown	n	x1	y1	...	xn	yn
 %	2	startup	shutdown	n	c(n-1)	...	c0
 mpc.gencost = [
-\t"""
-    write(io, text)
-    CSV.write(io, network.gencost; delim='\t', append=true, newline=";\n\t")
-    text = "];"
+$(_df2matpowermcsv(network.gencost))];"""
     write(io, text)
 end
 
