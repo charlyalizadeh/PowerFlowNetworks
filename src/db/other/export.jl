b70b11cd116f0a7de02604dcabbc1571bdcbffc4
charlyalizadeh <charlypro1502@outlook.fr>
2022-05-13 12:18:45 +0200

    Add exporting the database to be used with GNN

--- src/db/other/export.jl
+++ src/db/other/export.jl
@@ -8,21 +8,46 @@ end
 
 function decomposition_to_gnndata(row, graph, out)
     graph = loadgraph(row[:graph_path])
-    nv = nv(graph)
-    edges = [[e.src, e.dst] for e in edges(graph)]
+    edges = [[e.src, e.dst] for e in Graphs.edges(graph)]
+    open(out, "w") do io
+        JSON.print(io, edges)
+    end
+end
+
+function _row_to_dict(row)
+    return Dict(names(row) .=> values(row))
 end
 
 function db_to_gnndata(db, out)
     !isdir(out) && mkpath(out)
-    instances = DBInterface.execute(db, "SELECT * FROM instances") |> DataFrame
+    instances_path = joinpath(out, "instances")
+    decompositions_path = joinpath(out, "decompositions")
+    mkpath(instances_path)
+    mkpath(decompositions_path)
+    instances = DBInterface.execute(db, "SELECT * FROM instances WHERE pfn_path IS NOT NULL AND graph_path IS NOT NULL") |> DataFrame
     decompositions = DBInterface.execute(db, "SELECT * FROM decompositions") |> DataFrame
-    networks = [deserialize(p) for p in instances[!, :pfn_path]]
-    graphs = [loadgraph(g) for g in instances[!, :graph_path]]
-    for network in networks
-        bus_dict = Dict(network.bus[!, :ID] .=> network.bus[!, Not(:ID)])
-        gen_dict = Dict(network.gen[!, :ID] .=> network.gen[!, Not(:ID)])
-        branch_dict = Dict(network.branch[!, :ID] .=> network.branch[!, Not(:ID)])
-        gencost_dict = Dict(network.gencost[!, :ID] .=> network.gencost[!, Not(:ID)])
-        print(bus_dict)
+    networks = Dict("$(row[:name])_$(row[:scenario])" => deserialize(row[:pfn_path]) for row in eachrow(instances))
+    graphs = Dict(row[:id] => loadgraph(row[:graph_path]) for row in eachrow(instances))    
+    # Saving instances
+    for (name, network) in networks
+        # nodes
+        bus_dict = Dict(network.bus[!, :ID] .=> _row_to_dict.(eachrow(network.bus[!, Not(:ID)])))
+        gen_dict = Dict(network.gen[!, :ID] .=> _row_to_dict.(eachrow(network.gen[!, Not(:ID)])))
+        gencost_dict = Dict(network.gencost[!, :ID] .=> _row_to_dict.(eachrow(network.gencost[!, Not(:ID)])))
+        node_dict = merge(merge, bus_dict, gen_dict)
+        node_dict = merge(merge, node_dict, gencost_dict)
+
+        # edges
+        edges = [[row[:SRC], row[:DST]] for row in eachrow(network.branch)]
+        branch_dict = Dict(edges .=> _row_to_dict.(eachrow(network.branch[!, Not([:SRC, :DST])])))
+        final_dict = Dict("nodes" => node_dict, "edges" => branch_dict)
+        open(joinpath(instances_path, "$name.json"), "w") do io
+            JSON.print(io, final_dict)
+        end
     end
+
+    # Saving decompositions
+    to_ggnndata_function(row) = decomposition_to_gnndata(row, graphs[row[:origin_id]],
+                                                         joinpath(decompositions_path, "$(row[:origin_name])_$(row[:origin_scenario])_$(row[:uuid]).json"))
+    to_ggnndata_function.(eachrow(decompositions))
 end
