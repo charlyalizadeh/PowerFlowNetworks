6bada7ff0279df5e8b7c40bd412d909c7e6d7f72
charlyalizadeh <charlypro1502@outlook.fr>
2022-05-27 01:58:06 +0200

    Add `recompute` and `cholesky` options

--- scripts/julia/solve/solve.jl
+++ scripts/julia/solve/solve.jl
@@ -8,16 +8,23 @@ using ArgParse
 function parse_commandline()
     s = ArgParseSettings()
     import_general_settings!(s, ["db", "nbus_limit", "recompute", "mpi"])
+    @add_arg_table s begin
+        "--cholesky"
+            help = "Wether to solve only default Cholesky decomposition."
+            default = false
+            action = :store_true
+    end
     return parse_args(s)
 end
 
 function main()
     args = parse_commandline()
     db = SQLite.DB(args["dbpath"])
+    kwargs = strkey_to_symkey(args, ["recompute", "cholesky"])
     if args["mpi"]
-        execute_process_mpi(db, "solve_decompositions", args["log_dir"])
+        execute_process_mpi(db, "solve_decompositions", args["log_dir"]; kwargs...)
     else
-        solve_decompositions!(db)
+        solve_decompositions!(db; kwargs...)
     end
 end
 
